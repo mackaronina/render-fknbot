feca68113fb313e859899a0982cf7fd02d3e9976
mackaronina <116784694+mackaronina@users.noreply.github.com>
2024-01-06 03:08:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 bot.remove_webhook()
 bot.set_webhook(url=APP_URL, allowed_updates=['message',  'callback_query', 'chat_member'])
 
-cursor = create_engine('mysql+pymysql://68p1cik321lxjjz3bqdi:pscale_pw_yYTdRg4Y6mZXSZWIEaXrKlL4ibLCdGJ2hcVDFgKRftF@aws.connect.psdb.cloud:3306/nekodb', pool_recycle=280, connect_args={'ssl': {'ssl-mode': 'preferred'}})
+cursor = create_engine('mysql+pymysql://66nn9p23ephvw9myxy06:pscale_pw_AGbCXGz825Hwl09aFSeIHbE4KkCz3xTee3YDPtzn3Ko@aws.connect.psdb.cloud:3306/nekodb', pool_recycle=280, connect_args={'ssl': {'ssl-mode': 'preferred'}})
 db = []
 # ☣️
 
